b9309d4a78c93d07fc79922e43a0cb0f3fcdc53d
RenaufalAlrafid <renaufalalrafid@mail.ugm.ac.id>
2024-10-26 17:31:41 +0700

    fix : change tag in swager

--- src/main/java/com/lawencon/jobportal/controller/RoleController.java
+++ src/main/java/com/lawencon/jobportal/controller/RoleController.java
@@ -14,7 +14,7 @@ import com.lawencon.jobportal.service.RoleService;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.AllArgsConstructor;
 
-@Tag(name = "Auth", description = "Auth API endpoint")
+@Tag(name = "Role", description = "Auth API endpoint")
 @RestController
 @RequestMapping({"/api/v1"})
 @AllArgsConstructor
