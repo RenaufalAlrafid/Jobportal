a0def125e4e9d0860fb4ca518f7462ff0cf4003a
RenaufalAlrafid <renaufalalrafid@mail.ugm.ac.id>
2024-10-26 17:31:57 +0700

    make gender api

---
+++ src/main/java/com/lawencon/jobportal/controller/GenderController.java
@@ -0,0 +1,29 @@
+package com.lawencon.jobportal.controller;
+
+import java.util.List;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import com.lawencon.jobportal.authentication.helper.SessionHelper;
+import com.lawencon.jobportal.helper.ResponseHelper;
+import com.lawencon.jobportal.model.response.ConstantResponse;
+import com.lawencon.jobportal.model.response.WebResponse;
+import com.lawencon.jobportal.service.GenderService;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.AllArgsConstructor;
+
+@Tag(name = "Gender", description = "Auth API endpoint")
+@RestController
+@RequestMapping({"/api/v1"})
+@AllArgsConstructor
+public class GenderController {
+    private final GenderService service;
+
+    @GetMapping(value = "/gender", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<WebResponse<List<ConstantResponse>>> findAll() {
+        SessionHelper.validateRole("SA");
+        return ResponseEntity.ok(ResponseHelper.ok(service.getAll()));
+    }
+}
